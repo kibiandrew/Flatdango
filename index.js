60799e56a83e7700f7733d6a908fbfa2587a491e
Andrew Kibiwott <kibiandrew@gmail.com>
2022-12-16 23:29:52 +0300

    at work

--- index.js
+++ index.js
@@ -10,37 +10,46 @@ function openNav() {
     document.getElementById("main").style.marginLeft = "0";
   }
   //
-  let movie;
-document.addEventListener("DOMContentLoaded", () =>{
-  getFilm()
-  
-  })
-  //link to connect to the json db
-const getFilm =()=>{
-  fetch("http://localhost:3000/films")
-  .then(response => response.json())
-  .then(getMovie)
-}
-const infodiv = document.querySelector("filmTitle");
-  infodiv.innerHTML = character.name
-  const moviePoster =document.querySelector ("img#poster");
-  moviePoster.src =film.image;
-  const movieDescription=document.querySelector("#movieDescription")
-  film.innerHTML=film.movieDescription;
-  const runtime=document.querySelector("#runtime")
-  film.innerHTML=film.runtime;
-  const capacity=document.querySelector("#capacity")
-  film.innerHTML=film.capacity;
-  const showtime=document.querySelector("#showtime")
-  film.innerHTML=film.showtime;
-  const soldtickets=document.querySelector("#soldticket")
-  film.innerHTML=film.soldtickets;
+const films = [
+  {
+    "id": "1",
+    "title": "The Giant Gila Monster",
+    "runtime": "108",
+    "capacity": 30,
+    "showtime": "04:00PM",
+    "tickets_sold": 0,
+    "description": "A giant lizard terrorizes a rural Texas community and a heroic teenager attempts to destroy the creature.",
+    "poster": "https://www.gstatic.com/tv/thumb/v22vodart/2157/p2157_v_v8_ab.jpg"
+  },
+  {
+    "id": "2",
+    "title": "Manos: The Hands Of Fate",
+    "runtime": "118",
+    "capacity": 50,
+    "showtime": "06:45PM",
+    "tickets_sold": 0,
+    "description": "A family gets lost on the road and stumbles upon a hidden, underground, devil-worshiping cult led by the fearsome Master and his servant Torgo.",
+    "poster": "https://www.gstatic.com/tv/thumb/v22vodart/47781/p47781_v_v8_ac.jpg"
+  }
+];
 
-//   //calculate available tickets
-//   const capacity = document.getElementById("capacity")
-//   let tickets =Number(movie.capacity -Movie.tickets_sold)
-//  while (tickets >=1 && tickets <=30)=>{
-//   capacity-tickets;
-  
-//  }
- 
+const filmLinks = document.querySelectorAll('.sidebar a');
+const filmDetails = document.querySelector('.moviecard');
+const purchaseButton = document.querySelector('#purchase');
+
+filmLinks.forEach(link => {
+  link.addEventListener('click', event => {
+    // get the film title from the link
+    const filmTitle = event.target.textContent;
+    // find the film object in the films array
+    const film = films.find(film => film.title === filmTitle);
+    // update the film details on the page
+    filmDetails.querySelector('#filmTitle').textContent = film.title;
+    filmDetails.querySelector('#poster').src = film.poster;
+    filmDetails.querySelector('#movieDescription').textContent = film.description;
+    filmDetails.querySelector('#runtime').textContent = `Runtime: ${film.runtime} minutes`;
+    filmDetails.querySelector('#capacity').textContent = `Capacity: ${film.capacity}`;
+    filmDetails.querySelector('#showtime').textContent = `Showtime: ${film.showtime}`;
+    filmDetails.querySelector('#soldtickets').textContent = `Tickets sold: ${film.tickets_sold}`;
+  });
+});
